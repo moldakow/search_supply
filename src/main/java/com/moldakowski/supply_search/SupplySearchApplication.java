41b519a0b79c83959875617a0eb8fca13600f842
moldakow <moldakow@o2.pl>
2023-07-26 18:37:03 +0200

    pierwszy commit

---
+++ src/main/java/com/moldakowski/supply_search/SupplySearchApplication.java
@@ -0,0 +1,13 @@
+package com.moldakowski.supply_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplySearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moldakowski/supply_search/SupplySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldakowski.supply_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
